4319937e9c9647500209c1cade3cfcc0ad65587e
Matthias Metzger <mme@lambda9.de>
2024-03-12 21:54:44 +0100

    update deps and add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,31 @@
+import rss from '@astrojs/rss';
+import {getCollection} from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt('commonmark', {});
+
+export async function GET(context) {
+    const posts = await getCollection('posts');
+    return rss({
+        // `<title>` field in output xml
+        title: "Matthias' notes",
+        // `<description>` field in output xml
+        description: 'A humble Astronaut’s guide to the stars',
+        // Pull in your project "site" from the endpoint context
+        // https://docs.astro.build/en/reference/api-reference/#contextsite
+        site: context.site,
+        // Array of `<item>`s in output xml
+        // See "Generating items" section for examples using content collections and glob imports
+        items: posts.filter(post => !post.data.draft).map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.date,
+            description: post.data.summary,
+            content: sanitizeHtml(parser.render(post.body)),
+            // Compute RSS link from post `slug`
+            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            link: `/posts/${post.slug}/`,
+        })),
+        // (optional) inject custom xml
+        customData: `<language>en-us</language>`,
+    });
+}
